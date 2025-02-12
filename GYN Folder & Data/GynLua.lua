b2cd77b0f9d4ce85877d4dfbdf422ae156d6efc6
MasterGreyson <gpj6822@gmail.com>
2025-02-11 19:37:41 -0600

    Create GynLua

---
+++ GYN Folder & Data/GynLua.lua
@@ -0,0 +1,9 @@
+local GynAPI = http://github.com/MasterGreyson/GYN/blob/main
+local GYNPrompt = http://github.com/MasterGreyson/GYN/blob/main/README.md
+
+function GYN()
+GynAPI + GYNPrompt
+  
+end
+
+GYN()
